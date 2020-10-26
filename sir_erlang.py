76fa1f55e34e9faf080e928ff795c1c8e78b5062
Pol Pastells <polpastells@gmail.com>
2020-10-26 13:22:54 +0100

    Erlang for both infection and recovery

--- sir_erlang.py
+++ sir_erlang.py
@@ -24,7 +24,8 @@ parser.add_argument('--I0',type=int,default=20,help="Initial number of infected 
 parser.add_argument('--R0',type=int,default=0,help="Initial number of inmune individuals [0,N]")
 parser.add_argument('--delta',type=float,default=0.2,help="Mean ratio of recovery [1e-2,1]")
 parser.add_argument('--beta',type=float,default=0.5,help="Ratio of infection [1e-2,1]")
-parser.add_argument('--erlang_k',type=int,default=1,help="k parameter for the recovery time Erlang distribution, if set to 1 is an exponential distribution")
+parser.add_argument('--k_rec',type=int,default=1,help="k parameter for the recovery time Erlang distribution, if set to 1 is an exponential distribution")
+parser.add_argument('--k_inf',type=int,default=1,help="k parameter for the infection time Erlang distribution, if set to 1 is an exponential distribution")
 
 parser.add_argument('--llavor',type=int,default=1,help="Llavor from the automatic configuration")
 parser.add_argument('--data',type=str,default="italy_i.csv",help="File with time series")
@@ -38,9 +39,10 @@ args = parser.parse_args()
 # -------------------------
 # Parameters
 N = args.N
-erlang_k = args.erlang_k
-beta = args.beta/N
-delta = args.delta*erlang_k
+k_inf=args.k_inf
+k_rec=args.k_rec
+beta = args.beta/N*k_inf
+delta = args.delta*k_rec
 I0 = args.I0
 R0 = args.R0
 T_steps = int(1e7) # max simulation steps
@@ -50,7 +52,7 @@ seed0 = args.seed0
 infected_time_series = genfromtxt(args.data, delimiter=',')[args.day_min:args.day_max]
 
 print(args)
-print(infected_time_series)
+#print(infected_time_series)
 
 def beta_func(t):
     #t_conf = 20 # day of confinement
@@ -80,10 +82,10 @@ for seed in range(seed0,seed0+nseed):
 
     # -------------------------
     # Initialization
-    S,I,R = np.zeros(T_steps),np.zeros([T_steps,erlang_k]),np.zeros(T_steps)
-    I[0,0] = I0
+    S,I,R = np.zeros([T_steps,k_inf]),np.zeros([T_steps,k_rec]),np.zeros(T_steps)
+    S[0] = (N-I0-R0)/k_inf
+    I[0] = I0/k_rec
     R[0] = R0
-    S[0] = N-I0-R0
     I_day[mc_step,0]=I0
     R_day[mc_step,0]=R0
     #T = np.zeros(T_steps)
@@ -95,24 +97,29 @@ for seed in range(seed0,seed0+nseed):
     # -------------------------
     t,time,day,add_n=0,0,1,20
     while (I[t].sum()>0 and day<t_total-1):
+        #print(t,S[t],I[t],R[t])
         # Add individuals periodically
         #if(time//add_n==1):
             #add_n += 30
             #S[t] += float(N)/2
         if(time//day==1):
             day_max = max(day_max,day)
-            #S_day[mc_step,day]=S[t]
+            #S_day[mc_step,day]=S[t].sum()
             I_day[mc_step,day]=I[t].sum()
             R_day[mc_step,day]=R[t]
             day += 1
-        lambda_sum = (delta+beta_func(t)*S[t])*I[t].sum()
+        lambda_sum = (delta+beta_func(t)*S[t].sum())*I[t].sum()
         prob_heal = delta*I[t]/lambda_sum
+        prob_infect = beta_func(t)*S[t]*I[t].sum()/lambda_sum
         t+=1
         time += time_dist(lambda_sum)
         #T[t] = time
         random = np.random.random()
+        #print(prob_heal,prob_infect,random)
         end=False
-        for k in range(erlang_k-1):
+
+        # I(k)-> I(k+1)
+        for k in range(k_rec-1):
             if(random<prob_heal[:k+1].sum()):
                 S[t] = S[t-1]
                 I[t] = I[t-1]
@@ -120,16 +127,35 @@ for seed in range(seed0,seed0+nseed):
                 I[t,k+1] = I[t-1,k+1] + 1
                 R[t] = R[t-1]
                 end = True
-        if (end==True):
+        if(end==True):
             pass
+
+        # I(k)-> R
         elif(random<prob_heal.sum()):
             S[t] = S[t-1]
             I[t] = I[t-1]
-            I[t,erlang_k-1] = I[t-1,erlang_k-1] - 1
+            I[t,k_rec-1] = I[t-1,k_rec-1] - 1
             R[t] = R[t-1] + 1
+            end = True
+
+        # S(k)-> S(k+1)
+        else:
+            for k in range(k_inf-1):
+                if(random<(prob_heal.sum()+prob_infect[:k+1].sum())):
+                    S[t] = S[t-1]
+                    S[t,k] = S[t-1,k] - 1
+                    S[t,k+1] = S[t-1,k+1] + 1
+                    I[t] = I[t-1]
+                    R[t] = R[t-1]
+                    end = True
+        if(end==True):
+            pass
+
+        # S(k)-> I(0)
         else:
             # infect
-            S[t] = S[t-1] - 1
+            S[t] = S[t-1]
+            S[t,k_inf-1] = S[t-1,k_inf-1] -1
             I[t] = I[t-1]
             I[t,0] = I[t-1,0] + 1
             R[t] = R[t-1]
@@ -157,7 +183,7 @@ R_m = R_day.mean(0)
 #S_std = S_day.std(0)
 I_std = I_day.std(0)
 R_std = R_day.std(0)
-print(R_m[day_max],"Recovered individuals")
+#print(R_m[day_max],"Recovered individuals")
 plt.errorbar(np.arange(day_max),I_m[:day_max],yerr=I_std[:day_max],marker='o',ls='',label='I mean')
 
 I_m = np.median(I_day,0)
@@ -183,4 +209,4 @@ cost = 0
 for i in range(len(infected_time_series)):
     cost += (I_m[i]-infected_time_series[i])**2/(1+I_std[i])
 cost = np.sqrt(cost)
-print("GGA SUCCESS {}".format(cost))
+#print("GGA SUCCESS {}".format(cost))
