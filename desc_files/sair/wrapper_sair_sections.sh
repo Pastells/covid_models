f4ac0f2f77a64ddc017a9e3d7b2971be5f576c51
Pol Pastells <polpastells@gmail.com>
2021-06-10 16:46:54 +0200

    feat: ordered/updated description files

--- desc_files/sair/wrapper_sair_sections.sh
+++ desc_files/sair/wrapper_sair_sections.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-var_beta1="--beta1"
-var_beta2="--beta2"
-var_delta1="--delta1"
-var_delta2="--delta2"
-var_epsilon="--epsilon"
+var_beta="--beta"
+var_beta="--beta"
+var_delta_a="--delta_a"
+var_delta_a="--delta_a"
+var_alpha="--alpha"
 var_n="--n"
 other=""
 
@@ -15,35 +15,35 @@ while [[ $# -gt 0 ]]; do
         --beta1*)
         shift
         while [[ $1 != --* ]] && [[ $# -gt 0 ]]; do
-            var_beta1+=" $1"
+            var_beta+=" $1"
             shift
         done
         ;;
         --beta2*)
         shift
         while [[ $1 != --* ]] && [[ $# -gt 0 ]]; do
-            var_beta2+=" $1"
+            var_beta_a+=" $1"
             shift
         done
         ;;
         --delta1*)
         shift
         while [[ $1 != --* ]] && [[ $# -gt 0 ]]; do
-            var_delta1+=" $1"
+            var_delta+=" $1"
             shift
         done
         ;;
         --delta2*)
         shift
         while [[ $1 != --* ]] && [[ $# -gt 0 ]]; do
-            var_delta2+=" $1"
+            var_delta_a+=" $1"
             shift
         done
         ;;
         --epsilon*)
         shift
         while [[ $1 != --* ]] && [[ $# -gt 0 ]]; do
-            var_epsilon+=" $1"
+            var_alpha+=" $1"
             shift
         done
         ;;
@@ -66,4 +66,4 @@ while [[ $# -gt 0 ]]; do
 done
 
 ./scenario/venv/bin/python -u ./scenario/seir_erlang_sections.py \
-    $var_beta1 $var_beta2 $var_delta1 $var_delta2 $var_epsilon $var_n $other
+    $var_beta $var_beta_a $var_delta $var_delta_a $var_alpha $var_n $other
--- scripts/improving_gen.sh
+++ scripts/improving_gen.sh
@@ -2,21 +2,25 @@
 
 # prints which generations improve and their cost
 
-number=$1
-results="results/ac"  # results folder
 : ${1?"Usage: $0 number"}
+results="results/ac"  # results folder
 
-# save output
-# ./covid_ac stdout $number > $results/$number.stdout  # save output
-./scripts/save_problem.sh $number
+while [[ $# -gt 0 ]]; do
+    number=$1
+    echo $number
+    shift
+    # save output
+    # ./covid_ac stdout $number > $results/$number.stdout  # save output
+    ./scripts/save_problem.sh $number
 
-grep -B 6 "Generation Summary 1 " $results/$number.stdout > .temp.dat  # first generation
-grep -B 6 -A 30 "Winner has changed" $results/$number.stdout >> .temp.dat  # only the ones improving
-gen_array=($(grep "Generation Summary" .temp.dat | sed 's/^.*Summary //' | awk '{print $1;}'))
-cost_array=($(grep objective .temp.dat | sed 's/^.*objective: //'))
+    grep -B 6 "Generation Summary 1 " $results/$number.stdout > .temp.dat  # first generation
+    grep -B 6 -A 30 "Winner has changed" $results/$number.stdout >> .temp.dat  # only the ones improving
+    gen_array=($(grep "Generation Summary" .temp.dat | sed 's/^.*Summary //' | awk '{print $1;}'))
+    cost_array=($(grep objective .temp.dat | sed 's/^.*objective: //'))
 
-echo "gen cost"
-for ((i=1; i<=${#gen_array[@]}; i++)); do
-    echo "${gen_array[i]} ${cost_array[i]}"
+    echo "gen cost"
+    for ((i=1; i<=${#gen_array[@]}; i++)); do
+        echo "${gen_array[i]} ${cost_array[i]}"
+    done
 done
 rm .temp.dat
