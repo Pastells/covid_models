28d7d48a87872e738897c17c55ea76f6353fafd5
Josep <josep.a.95@gmail.com>
2021-06-11 10:54:59 +0200

    feat: added net_sair model entrypoint fix: corrected parameters for sair and sird models entrypoints

--- models/optilog_entrypoints.py
+++ models/optilog_entrypoints.py
@@ -1,6 +1,6 @@
 import numpy
 
-from .sair import sair
+from .sair import sair, net_sair
 from .utils import config
 from .sird import sird
 
@@ -25,7 +25,7 @@ def _auto_sird(dataset, seed):
     mc_nseed = 100
 
     t_total = day_max - day_min
-    time_series = load_data(dataset, day_max, day_min)
+    time_series = load_data(dataset, day_min, day_max)
 
     cost = sird.sird(
         time_series=time_series,
@@ -45,7 +45,7 @@ def _auto_sair(dataset, seed):
     mc_nseed = 100
 
     t_total = day_max - day_min
-    time_series = load_data(dataset, day_max, day_min)
+    time_series = load_data(dataset, day_min, day_max)
 
     cost = sair.sair(
         time_series=time_series,
@@ -58,9 +58,29 @@ def _auto_sair(dataset, seed):
     _report_result(cost)
 
 
+def _auto_net_sair(dataset, seed):
+    # Constants:
+    day_min = 0
+    day_max = 54
+    mc_nseed = 100
+
+    t_total = day_max - day_min
+    time_series = load_data(dataset, day_min, day_max)
+
+    cost = net_sair.net_sair(
+        time_series=time_series,
+        seed=seed,
+        n_seeds=mc_nseed,
+        t_total=t_total,
+        metric="models.utils.utils.sum_sq"
+    )
+    _report_result(cost)
+
+
 _entrypoints = {
     "sird": (_auto_sird, [sird.sird]),
-    "sair": (_auto_sair, [sair.sair])
+    "sair": (_auto_sair, [sair.sair]),
+    "net_sair": (_auto_net_sair, [net_sair.net_sair])
 }
 
 
